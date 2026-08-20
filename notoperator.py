5edd45538d3600e6c7cd651e68fddd9b1e6fbb6c
shreyankguduru-tech <shreyank.guduru@gmail.com>
2026-08-20 20:02:01 +1000

    initial commit

---
+++ notoperator.py
@@ -0,0 +1,14 @@
+
+
+a = 12
+b = 15
+
+print(not(a == b))
+
+a = "Python"
+b = "coding"
+
+if (not(a == b)) == True:
+    print (a,"and", b, "are not the same")
+else:
+    print (a, "and", b, "are the same ")
