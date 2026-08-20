1a2833d1046234eebe82eb212d6aadd6e455013c
shreyankguduru-tech <shreyank.guduru@gmail.com>
2026-08-20 19:52:10 +1000

    initial commit

---
+++ AndOrStatements.py
@@ -0,0 +1,17 @@
+a = 13
+b = -20
+c = 8
+
+if a > 0 and b > 0 and c > 0:
+    print ("All the numbers are greater than 0")
+else:
+    print ("atleast one number is not greater than 0")
+
+
+e = 12
+f = 20
+
+if e > 15 or f > 15:
+    print ("Either one is correct")
+else:
+    print ("Both of them is false")
